be6f85486abedaff9407e6d757e7cf16792e082e
freddycastro11 <Freddyenrique2000@gmail.com>
2020-04-29 16:27:48 -0600

    xd

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,17 +1,14 @@
 function init() {
-    $("[class*='mostrar']").click(function(e){
-            let modulo = $(this).data("modulo"),
-                form = $(this).data("form");
-          //  fetch(`public/vistas/${modulo}/${form}.html`).then(resp => resp.text()).then(resp => {
-                $(`#vista-${form}`).load(`Public/vistas/${modulo}/${form}.html`, function () {
-                 $(`#btn-close-${form}`).click(()=>{
-                    $(`#vista-${form}`).html("");
-                });
-                //import(`../vistas/${modulo}/${form}.js`).then(module => {
-                    //module.modulo();
-                //});
-                init();
-            }).draggable();
-        });
+    $("[class*='mostrar']").click(function (e) {
+        let modulo = $(this).data("modulo"),
+            form = $(this).data("form");
+
+        $(`#vista-${form}`).load(`public/vistas/${modulo}/${form}.html`, function () {
+            $(`#btn-close-${form}`).click(() => {
+                $(`#vista-${form}`).html("");
+            });
+            init();
+        }).draggable();
+    });
 }
 init();
