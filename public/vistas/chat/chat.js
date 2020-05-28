ed70078691d85f91387931a99923bf6aa6695139
Tania_Bonilla <t.a.b.a.tania@hotmail.com>
2020-05-27 20:52:33 -0600

    chat avance

--- public/vistas/chat/chat.js
+++ public/vistas/chat/chat.js
@@ -28,3 +28,26 @@ var socket = io.connect("http://localhost:3001",{'forceNew':true}),
             appchat.msgs.push(item.msg);
         });
     });
+    (function() {
+	
+
+	
+        $('#live-chat header').on('click', function() {
+    
+            $('.chat').slideToggle(300, 'swing');
+            $('.chat-message-counter').fadeToggle(300, 'swing');
+    
+        });
+        
+    
+        $('.chat-close').on('click', function(e) {
+    
+            e.preventDefault();
+            $('#live-chat').fadeOut(300);
+    
+        });
+        
+    }) ();
+    
+    
+        
