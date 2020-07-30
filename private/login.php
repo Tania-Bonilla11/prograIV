eff9f8b417bddf83deecfdb2f58b9a0090f20332
Angel Bran <angelbranm@gmail.com>
2020-07-30 09:20:50 -0600

    Cambios en conexion, login, registro y privilegios

--- private/login.php
+++ private/login.php
@@ -12,39 +12,37 @@
         $clave = $_POST['clave'];
         $clave = hash('sha512', $clave);
        
-        
         if (empty($usuario)){
             $error .= '<i>Favor de ingresar el usuario</i>';
         } 
         else if (empty(trim ($_POST['clave']))){
             $error .= '<i>Favor de ingresar la clave</i>';
-        }else{
-        try{
-            $conexion = new PDO('mysql:host=localhost;dbname=db_eduvial', 'root', '');
-            }catch(PDOException $prueba_error){
-                echo "Error: " . $prueba_error->getMessage();
-            }
-        
-        $statement = $conexion->prepare('
-        SELECT * FROM login WHERE usuario = :usuario AND clave = :clave '
-        );
-        
-        $statement->execute(array(
-            ':usuario' => $usuario,
-            ':clave' => $clave,
-            
-        ));
-            
-        $resultado = $statement->fetch();
-        
-        if ($resultado !== false){
-            $_SESSION['usuario'] = $usuario;
-            if(isset($_SESSION['usuario'])){
-               header('location: principal/principal.php');
-            }
-        }else{
-            $error .= '<i>Este usuario no existe</i>';
         }
-    }
+        else {
+            // Cambio en la conexion a la base de datos a Conexion/DB.php
+            include_once('Conexion/DB.php');
+            
+            $statement = $conexion->prepare('
+            SELECT id, privilegio FROM login WHERE usuario = :usuario AND clave = :clave '
+            );
+            
+            $statement->execute(array(
+                ':usuario' => $usuario,
+                ':clave' => $clave,
+            ));
+                
+            $resultado = $statement->fetch(PDO::FETCH_OBJ);
+            
+            if ($resultado !== false){
+                $_SESSION['usuario'] = $usuario;
+                $_SESSION['privilegio'] = $resultado->privilegio;
+
+                if(isset($_SESSION['usuario'])){
+                    header('location: principal/principal.php');
+                }
+            }else{
+                $error .= '<i>Este usuario no existe</i>';
+            }
+        }
 }
 require '../public/frontend/login-vista.php';
--- private/register.php
+++ private/register.php
@@ -40,24 +40,20 @@
         else if (strlen($_POST['clave']) < 8) {
             $error .= '<i>la contraseña Es Demasiada Corta</i>'; 
         } 
-        else if (strlen($_POST['clave']) > 12) {
+        else if (strlen($_POST['clave']) > 16) {
             $error .= '<i>la contraseña Es Demasiada larga</i>'; 
         }
         else if (empty($_POST['clave2'])){
-            $error .= '<i>Favor ingresar la contraseña de confirmacion</i>';
+            $error .= '<i>Favor ingresar la contraseña de confirmación</i>';
         
         }else{
-            try{
-                $conexion = new PDO('mysql:host=localhost;dbname=db_eduvial', 'root', '');
-            }catch(PDOException $prueba_error){
-                echo "Error: " . $prueba_error->getMessage();
-            }
+            // Cambio en la conexion a la base de datos a Conexion/DB.php
+            include_once('Conexion/DB.php');
             
             $statement = $conexion->prepare('SELECT * FROM login WHERE usuario = :usuario LIMIT 1');
             $statement->execute(array(':usuario' => $usuario));
             $resultado = $statement->fetch();
-            
-                        
+                     
             if ($resultado != false){
                 $error .= '<i>Este usuario ya existe</i>';
             }
@@ -65,21 +61,25 @@
             if ($clave != $clave2){
                 $error .= '<i> Las contraseñas no coinciden</i>';
             }
-            
-            
         }
         
         if ($error == ''){
-            $statement = $conexion->prepare('INSERT INTO login (id, correo, usuario, clave) VALUES (null, :correo, :usuario, :clave)');
+            $statement = $conexion->prepare('INSERT INTO login (correo, usuario, clave, privilegio) VALUES (:correo, :usuario, :clave, 0)');
+
             $statement->execute(array(
-                
                 ':correo' => $correo,
                 ':usuario' => $usuario,
                 ':clave' => $clave
-                
             ));
+
+            $arr = $statement->errorInfo();
             
-            $error .= '<i style="color: green;">Usuario registrado exitosamente</i>';
+            if (!empty($arr) && $arr[1] == 1) {
+                $error .= $arr[2];
+            }
+            else {
+                $error .= '<i style="color: green;">Usuario registrado exitosamente</i>';
+            }
         }
     }
 
